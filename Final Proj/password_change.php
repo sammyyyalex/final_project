f958ec84bb8a3b53c6166b13526ea6d04425b16d
lonialnor <70921285+lonialnor@users.noreply.github.com>
2021-04-24 16:54:35 -0400

    Add files via upload

--- Final Proj/password_change.php
+++ Final Proj/password_change.php
@@ -2,91 +2,65 @@
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
 <title>Password Change</title>
-</head>
-      
-
-
-<body>
-       <h1>Change Password</h1>
-      <form method="POST" action="password_change.php">
-      <script src="password_change.js"></script>
-       <table>
-       <tr>
-      <td>Enter your UserName</td>
-       <td><input type="username" size="10" name="userName"></td>
-       </tr>
-       <tr>
-       <td>Enter your existing password:</td>
-       <td><input type="password" size="10" name="userPassword"></td>
-       </tr>
-     <tr>
-       <td>Enter your new password:</td>
-       <td><input type="password" size="10" name="newpassword"></td>
-       </tr>
-       <tr>
-      <td>Re-enter your new password:</td>
-      <td><input type="password" size="10" name="confirmnewpassword"></td>
-       </tr>
-       </table>
-       <p><input type="submit" value="Update Password">
-       </form>
-      
+     </head>
+    <body>
+    <h1>Change Password</h1>
+   <form method="POST" action="password_change.php">
+    <table>
+    <tr>
+   <td>Enter your UserName</td>
+    <td><input type="username" size="10" name="username"></td>
+    </tr>
+    <tr>
+    <td>Enter your existing password:</td>
+    <td><input type="password" size="10" name="password"></td>
+    </tr>
+  <tr>
+    <td>Enter your new password:</td>
+    <td><input type="password" size="10" name="newpassword"></td>
+    </tr>
+    <tr>
+   <td>Re-enter your new password:</td>
+   <td><input type="password" size="10" name="confirmnewpassword"></td>
+    </tr>
+    </table>
+    <p><input type="submit" value="Update Password">
+    </form>
 
       </body>
 </html>
 
 
 
-
-
 <?php
-require ('db.php');
-class connect_database{
-    private $hostname = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $dbname = "Final_Project"; // fill in your database name containing tables: products and categories
-    public $conn = NULL;
+require "db.php";
 
-    public function connectDb(){
-        try{
-            $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->dbname", $this->username, $this->password);
-			//if($this->conn !=null){echo "successfully connected";}
-        }
-        catch(PDOException $e){
-            $error_out->http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n". $e->getMessage());
-        }
-        return $this->conn;
-    }
-}
-
-
-$username = $_POST['userName'];
-        $password = $_POST['userPassword'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
         $newpassword = $_POST['newpassword'];
         $confirmnewpassword = $_POST['confirmnewpassword'];
-        $result = mysql_query("SELECT userPassword FROM users WHERE userID='$userName'");
-        
-        if(!$result) {
-            echo "The username you entered does not exist";
+        $result = mysql_query("SELECT userPassword FROM users WHERE userName='$username'");
+        if(!$result){
+        echo "The username you entered does not exist";
+        }
+        else if($password!= mysql_result($result, 0))
+        {
+        echo "You entered an incorrect password";
         }
         
-        else if($password!= mysql_result($result, 0)) {
-            echo "You entered an incorrect password";
+        if($newpassword==$confirmnewpassword){
+        $sql=mysql_query("UPDATE users SET password='$newpassword' WHERE userName='$username'"); // somewhere i have to put in the password checker
         }
 
-        if($newpassword==$confirmnewpassword)
-        $sql=mysql_query("UPDATE users SET userPassword='$newpassword' where 
-        userID='$userName'");
-        if($sql) {
-            echo "Congratulations You have successfully changed your password";
-        }else{
-            echo "Passwords do not match";
+        if($sql){
+        echo "Congratulations You have successfully changed your password";
+        }
+       else{
+       echo "Passwords do not match";
        }
-      ?>
-}
-<!-- ok so im pretty sure the if else statement is scuffed -->
+
 
 ?>
 
--- Final Proj/user_change.php
+++ Final Proj/user_change.php
@@ -1,1 +1,65 @@
- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<title>Username Change</title>
+     </head>
+    <body>
+    <h1>Change Username</h1>
+   <form method="POST" action="password_change.php">
+    <table>
+    <tr>
+   <td>Enter your Password</td>
+    <td><input type="password" size="10" name="password"></td>
+    </tr>
+    <tr>
+    <td>Enter your existing username:</td>
+    <td><input type="password" size="10" name="username"></td>
+    </tr>
+  <tr>
+    <td>Enter your new username:</td>
+    <td><input type="username" size="10" name="newusername"></td>
+    </tr>
+    <tr>
+   <td>Re-enter your new username:</td>
+   <td><input type="username" size="10" name="confirmnewusername"></td>
+    </tr>
+    </table>
+    <p><input type="submit" value="Update Username">
+    </form>
+
+      </body>
+</html>
+
+
+
+<?php
+require "db.php";
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $newpassword = $_POST['newusername'];
+        $confirmnewpassword = $_POST['confirmnewusername'];
+        $result = mysql_query("SELECT userName FROM users WHERE userPassword='$password'");
+        if(!$result){
+        echo "The password does not match the username that exists!";
+        }
+        else if($password!= mysql_result($result, 0))
+        {
+        echo "You entered an incorrect password";
+        }
+        
+        if($newusername==$confirmnewusername){
+        $sql=mysql_query("UPDATE users SET userName='$newusername' WHERE userPassword='$password'"); // somewhere i have to put in the username checker
+        }
+
+        if($sql){
+        echo "Congratulations You have successfully changed your username!";
+        }
+       else{
+       echo "Password does not exist";
+       }
+
+
+?>
