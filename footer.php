17df420992cfdabacad31adc39ff7f098b88c05b
cindygonzalezg <cmg23@njit.edu>
2021-04-27 17:39:20 -0400

    Added welcome,header,logout,main,footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> My Student Planner, Inc.
+    </p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Student Planner</title>
+    <link rel="stylesheet" type="text/css" href="./login/main.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Student Planner</h1></header>
+
